4c8cb4b9d86a87331865ad6781488f1899b649f8
Bruce Atkinson <bruce@appinlet.com>
2018-08-24 01:14:21 +0200

    Initial commit

---
+++ woocommerce-gateway-paysubs/gateway-paysubs.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Plugin Name: PayGate PaySubs plugin for WooCommerce
+ * Plugin URI: https://www.paygate.co.za
+ * Description: Accept payments for WooCommerce using PayGate's PaySubs service
+ * Version: 1.0.0
+ * Author: PayGate (Pty) Ltd
+ * Author URI: https://www.paygate.co.za/
+ * Developer: App Inlet (Pty) Ltd
+ * Developer URI: https://www.appinlet.com/
+ *
+ * WC requires at least: 2.6
+ * WC tested up to: 3.3
+ *
+ * Copyright: © 2018 PayGate (Pty) Ltd.
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+/**
+ * Required functions
+ */
+if ( !function_exists( 'woothemes_queue_update' ) ) {
+    require_once 'woo-includes/woo-functions.php';
+}
+
+add_action( 'plugins_loaded', 'woocommerce_paysubs_init', 0 );
+
+function woocommerce_paysubs_init()
+{
+    if ( !class_exists( 'WC_Payment_Gateway' ) ) {return;}
+    require_once plugin_basename( 'classes/paysubs.class.php' );
+    add_filter( 'woocommerce_payment_gateways', 'woocommerce_paysubs_add_gateway' );
+} // End woocommerce_paysubs_init()
+
+function woocommerce_paysubs_add_gateway( $methods )
+{
+    $methods[] = 'WC_Gateway_PaySubs';return $methods;
+} // End woocommerce_paysubs_add_gateway()
---
+++ woocommerce-gateway-paysubs/readme.txt
@@ -0,0 +1,7 @@
+=== WooCommerce PayGate PaySubs Gateway ===
+
+A payment gateway for PayGate. A PaySubs terminal ID is required for this gateway to function.
+
+== Important Note ==
+
+An SSL certificate is recommended for additional safety and security for your customers.
