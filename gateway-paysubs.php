ef52de4b3f9b78b839b4c51a427837d3c8e8a14e
Bruce Atkinson <bruce@appinlet.com>
2019-01-02 14:37:01 +0200

    = 1.0.3 - December 28, 2018 = * Add auto-update feature

--- gateway-paysubs.php
+++ gateway-paysubs.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Plugin Name: PayGate PaySubs plugin for WooCommerce
- * Plugin URI: https://www.paygate.co.za
+ * Plugin URI: https://github.com/PayGate/PaySubs_WooCommerce
  * Description: Accept payments for WooCommerce using PayGate's PaySubs service
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: PayGate (Pty) Ltd
  * Author URI: https://www.paygate.co.za/
  * Developer: App Inlet (Pty) Ltd
@@ -28,12 +28,38 @@ add_action( 'plugins_loaded', 'woocommerce_paysubs_init', 0 );
 
 function woocommerce_paysubs_init()
 {
-    if ( !class_exists( 'WC_Payment_Gateway' ) ) {return;}
+    if ( !class_exists( 'WC_Payment_Gateway' ) ) {
+        return;
+    }
     require_once plugin_basename( 'classes/paysubs.class.php' );
     add_filter( 'woocommerce_payment_gateways', 'woocommerce_paysubs_add_gateway' );
+
+    require_once 'classes/updater.class.php';
+
+    if ( is_admin() ) {
+        // note the use of is_admin() to double check that this is happening in the admin
+        $config = [
+            'slug'               => plugin_basename( __FILE__ ),
+            'proper_folder_name' => 'woocommerce-gateway-paysubs',
+            'api_url'            => 'https://api.github.com/repos/PayGate/PaySubs_WooCommerce',
+            'raw_url'            => 'https://raw.githubusercontent.com/PayGate/PaySubs_WooCommerce/master',
+            'github_url'         => 'https://github.com/PayGate/PaySubs_WooCommerce',
+            'zip_url'            => 'https://github.com/PayGate/PaySubs_WooCommerce/archive/master.zip',
+            'homepage'           => 'https://github.com/PayGate/PaySubs_WooCommerce',
+            'sslverify'          => true,
+            'requires'           => '4.0',
+            'tested'             => '4.9.8',
+            'readme'             => 'README.md',
+            'access_token'       => '',
+        ];
+
+        new WP_GitHub_Updater( $config );
+
+    }
 } // End woocommerce_paysubs_init()
 
 function woocommerce_paysubs_add_gateway( $methods )
 {
-    $methods[] = 'WC_Gateway_PaySubs';return $methods;
+    $methods[] = 'WC_Gateway_PaySubs';
+    return $methods;
 } // End woocommerce_paysubs_add_gateway()
